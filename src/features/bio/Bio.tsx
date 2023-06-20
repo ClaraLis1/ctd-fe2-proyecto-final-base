15edb7ea0beafc93bd849e728b4bdbd133bd577d
Your Name <lisleclara1@gmail.com>
2023-06-20 12:14:36 -0300

    added styled component

--- src/features/bio/Bio.tsx
+++ src/features/bio/Bio.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { NombresSimpsons, INFO_SIMPSONS } from "./constants";
-import styles from "./styles.module.css";
 import { Buttons, ContainerBio, ContenedorBotonesBio, DescripcionBio, ImageBio, NombreBio } from "./styled";
 
 const Bio = () => {
@@ -13,26 +12,12 @@ const Bio = () => {
 
   const crearBotones = () => {
     return Object.keys(INFO_SIMPSONS).map((nombre: string) => (
-      <>
-      {/* <button
-        key={nombre as string}
-        onClick={() => onClick(nombre as NombresSimpsons)}
-        className={
-          bioActiva.id === nombre
-            ? styles.botonBioActivo
-            : styles.botonBioInactivo
-        }
-      >
-        {nombre}
-      </button> */}
       <Buttons  
         key={nombre as string}
         onClick={() => onClick(nombre as NombresSimpsons)}
         primary= { bioActiva.id === nombre}>
           {nombre}
-      </Buttons>
-      </> 
-      
+      </Buttons>            
     ));
   };
 
@@ -52,24 +37,8 @@ const Bio = () => {
         <NombreBio>{bioActiva.nombre}</NombreBio>
         <DescripcionBio>{bioActiva.descripcion}</DescripcionBio>
       </div>
+      </>
 
-    {/* <div className={styles.bioContainer}>
-      <div className={styles.contenedorBotones}>{crearBotones()}</div>
-      <div>
-        <div>
-          <img
-            src={bioActiva.image}
-            alt={bioActiva.nombre}
-            className={styles.bioImagen}
-          />
-        </div>
-        <div>
-          <h3 className={styles.bioNombre}>{bioActiva.nombre}</h3>
-          <p className={styles.bioDescripcion}>{bioActiva.descripcion}</p>
-        </div>
-      </div>
-    </div> */}
-    </>
   );
 };
 
